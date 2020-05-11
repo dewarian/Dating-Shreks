94575da9f87b728bb6eaf4d4d4c51a65d8471495
ParvinBDJ <55492595+ParvinBDJ@users.noreply.github.com>
2020-05-11 10:05:01 +0200

    Add files via upload
    I couldn't upload my _node_modules_ for as they were too big.

---
+++ back-end/week1/java.js
@@ -0,0 +1,7 @@
+const camelCase = require('camelcase');
+
+console.log(camelCase("ik ben parvin en heel erg leuk"))
+
+var ewa = require('lodash');
+
+console.log(ewa("ik ben parvin en heel erg leuk)"))
