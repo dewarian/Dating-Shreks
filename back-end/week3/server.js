806d92232183e52f654d07b450d69e4c3bf3bbf8
ParvinBDJ <55492595+ParvinBDJ@users.noreply.github.com>
2020-06-01 20:19:24 +0200

    Add files via upload

--- back-end/week3/server.js
+++ back-end/week3/server.js
@@ -19,17 +19,17 @@ const url = process.env.MONGO_URL;
 const app = express();
 const port = process.env.PORT || 3000;
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
-const nameID = 'sid';
+const nameID = 'nameID';
 let db = null;
+let user = null;
 const sessionSecret = process.env.SESSION_SECRET;
-const storeThing = new MongoDBStore({ // Hulp van Victor
+const storeThing = new MongoDBStore({ // Hulp van Victor Boucher
   uri: url,
   collection: 'session',
 });
 storeThing.on('error', (err) => {
-  console.log('error with sessions' + err);
+  console.log('error with sessions');
 });
-
 
 app.use(session({
   name: nameID,
@@ -43,7 +43,6 @@ app.use(session({
   },
 }));
 
-
 function listen() {
   // eslint-disable-next-line no-console
   console.log('app started at port:', port);
@@ -55,9 +54,23 @@ app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use(express.static('website'));
 
-function home(request, respone) {
-  respone.render('form.handlebars');
+function home (request, response) {
+  mongodb.MongoClient.connect(url, (err, client) => {
+      if (err) {
+          throw err;
+      } else {
+          db = client.db(process.env.DB_NAME);
+          user = db.collection('user');
+          response.render('form');
+          user.findOne({
+              _id: request.session.nameID
+          }, (err, user) => {
+              console.log(user.name);
+          });
+      }
+  });
 }
+
 app.get('/', urlencodedParser, home);
 
 
@@ -68,13 +81,13 @@ app.post('/volgendeFilm-succes', urlencodedParser, volgendeFilm);
 
 
 function succesMan(request, response) {
+  console.log(request.session.nameID);
   response.render('succes', { data: request.body });
 }
+
 app.post('/succes', urlencodedParser, succesMan);
 app.get('/succes', urlencodedParser, succesMan);
 
-
-// Below I try to run the database and connect with it
 function form(request, response) {
   console.log(request.session);
   response.render('name');
@@ -85,16 +98,25 @@ function addName(req, res, next) {
   mongodb.MongoClient.connect(url, (err, client) => {
     if (err) {
       throw err;
+    } else {
+      db = client.db(process.env.DB_NAME);
+      user = db.collection('user');
+      user.insertOne(
+        {
+          name: req.body.firstname,
+        },
+      );
+      const nameNow = req.body.firstname;
+      user.findOne({ name: nameNow }, function(err, user) {
+        if(err) {
+          console.log('It is not working');
+        } else {
+          req.session.nameID = user._id;
+          res.redirect('succes');
+        }
+      });
     }
-    db = client.db(process.env.DB_NAME);
-    const user = db.collection('user');
-    user.insertOne(
-      {
-        name: req.body.firstname,
-      },
-    );
-    // eslint-disable-next-line no-use-before-define
-    done();
+    // done();
   });
 
   function done(err) {
