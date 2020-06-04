eb1ac74052ee77c483e6d3a700e4ca55799dac9e
ParvinBDJ <55492595+ParvinBDJ@users.noreply.github.com>
2020-06-04 22:23:18 +0200

    Delete java.js

--- back-end/week1/java.js
+++
@@ -1,7 +0,0 @@
-const camelCase = require('camelcase');
-
-console.log(camelCase("ik ben parvin en heel erg leuk"))
-
-var ewa = require('lodash');
-
-console.log(ewa("ik ben parvin en heel erg leuk)"))
