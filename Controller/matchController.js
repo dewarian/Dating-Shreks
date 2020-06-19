9258eef2add8f798a1c1f8b0b196057d98727f75
Nathan <dewakik@gmail.com>
2020-06-19 14:17:58 +0200

    [update] removed deleting. It is overrated.

--- Controller/matchController.js
+++ Controller/matchController.js
@@ -6,8 +6,6 @@ const mongodb = require('mongodb');
 const getUsers = require('./modules/getUser');
 const url = process.env.MONGO_URL;
 const nameID = 'nameID';
-const ObjectId = require('mongodb').ObjectId;
-
 
 
 router.use(bodyParser.urlencoded({
@@ -16,33 +14,10 @@ router.use(bodyParser.urlencoded({
 
 router.get('/', (req, res) => {
   getUsers.getUsers(req, res);
+  console.log(`session: ${JSON.stringify(req.session)}`)
+  console.log(`body: ${JSON.stringify(req.body.users)}`)
+  let sess = req.session.users = [];
+  sess.push();
 })
-
-router.post('/delete', (req, res) =>{
-  console.log(`${JSON.stringify(res.body)}`)
-  console.log(`trying to delete: ${req.body.id}`);
-  mongodb.MongoClient.connect(url, {
-    useUnifiedTopology: true
-  }, function(err, client) {
-    if (err) {
-      console.error(`[MONGO ERR]: ${err}`);
-    } else {
-      db = client.db(process.env.DB_NAME);
-      user = db.collection('user');
-      user.deleteMany({_id: new ObjectId(req.body.id)}), function(err, result){
-        if(err) throw console.log(`failure: ${err}`);
-        console.log(`trying to delete: ${req.body.id}`);
-        user.find().toArray(function(err, result) {
-          if (err) throw console.log(`[MONGO ERR]: \n ${err}`)
-          userData = result;
-          res.render('users', {
-            title: 'Shreks App',
-            users: userData
-          });
-        })
-      }
-    }
-  })
-});
 
 module.exports = router;
--- Controller/modules/getUser.js
+++ Controller/modules/getUser.js
@@ -25,36 +25,7 @@ function getUsers(req, res) {
   })
 }
 
-function deleteUser(req, res) {
-  console.log(`trying to delete: ${req.body.id}`);
-  mongodb.MongoClient.connect(url, {
-    useUnifiedTopology: true
-  }, function(err, client) {
-    if (err) {
-      console.error(`[MONGO ERR]: ${err}`);
-    } else {
-      db = client.db(process.env.DB_NAME);
-      user.deleteMany({_id: new ObjectId(req.body.id)}), function(err, result){
-        if(err) throw console.log(`failure: ${err}`);
-        console.log(`trying to delete: ${req.body.id}`);
-        user.find().toArray(function(err, result) {
-          if (err) throw console.log(`[MONGO ERR]: \n ${err}`)
-          userData = result;
-          res.render('users', {
-            title: 'Shreks App',
-            users: userData
-          });
-        })
-      }
-    }
-  })
-}
-
-
-// exports.getUsers = getUsers;
-// module.exports = {getUsers, goToUsers};
 exports.userData = userData;
 module.exports = {
   getUsers,
-  deleteUser,
 };
