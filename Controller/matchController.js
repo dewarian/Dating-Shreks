07d53183df56793b5bd7451f686d43d941ffd61a
ParvinBDJ <55492595+ParvinBDJ@users.noreply.github.com>
2020-06-21 23:02:38 +0200

    Add files via upload

--- Controller/matchController.js
+++ Controller/matchController.js
@@ -4,45 +4,46 @@ const router = express.Router();
 const mongodb = require('mongodb');
 const getUsers = require('./modules/getUser');
 const url = process.env.MONGO_URL;
-const api = require('./modules/getAPI');
-
 
 router.use(bodyParser.urlencoded({
   extended: false,
 }));
 
+let db = '""';
+let user = '';
+
 router.get('/', (req, res) => {
   if (!req.session.nameID) {
-    res.render('name')
+    res.render('name');
   } else {
     getUsers.getUsers(req, res);
   }
-})
+});
 
 router.get('/user', (req, res) => {
   if (!req.session.nameID) {
-    res.render('name')
+    res.render('name');
   } else {
-  mongodb.MongoClient.connect(url,  {useUnifiedTopology: true}, (err, client) => {
-    if (err) {
-      console.error(`[MONGO ERR]: ${err}`);
-    } else {
-      db = client.db(process.env.DB_NAME);
-      user = db.collection('user');
-      user.find().toArray((err, result) => {
-        if (err) throw err;
-        res.render('users', {users: result})
-      });
-    }
-  })
-}
-})
+    mongodb.MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
+      if (err) {
+        console.error(`[MONGO ERR]: ${err}`);
+      } else {
+        db = client.db(process.env.DB_NAME);
+        user = db.collection('user');
+        user.find().toArray((err, result) => {
+          if (err) throw err;
+          res.render('users', {users: result});
+        });
+      }
+    });
+  }
+});
 
 router.post('/filtered', (req, res) => {
   if (!req.session.nameID) {
-    res.render('name')
-  }else {
-    getUsers.filterUsers(req, res)
+    res.render('name');
+  } else {
+    getUsers.filterUsers(req, res);
   }
 });
 
